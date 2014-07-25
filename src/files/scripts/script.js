0f1cd0ba0a5b7cce8ad0a0c4f8d5fff32cf7958b
Mikeumus <mike@betabulls.com>
2014-07-25 16:27:14 -0700

    Scroll animation iteration.

--- src/files/scripts/script.js
+++ src/files/scripts/script.js
@@ -1,9 +1,9 @@
 $(function(){
-  
+  $("#stage").toggle();
   $(".hex-btn-space").on('click', function(){
     $("#head").slideToggle();
     $("section.body").fadeToggle();
-    $("#StageOne").fadeToggle();
+    $("#stage").toggle();
     $(this).toggleClass('how-hex');
     $("body").toggleClass('orange-body');
   });
