25760b01560d9ac49741bf11e444a42f4459552c
Jennifer M <nevermuses@gmail.com>
2014-08-12 09:20:42 -0400

    55bc856 Mobile adjustments.

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+$(function(){
+  
+  $("#stage").toggle();
+  $(".hex-btn-space").on('click', function(){
+    $("#head").slideToggle();
+    $("section.body").fadeToggle();
+    $("#stage").toggle();
+    $(this).toggleClass('how-hex');
+    $("body").toggleClass('orange-body');
+  });
+  
+});
